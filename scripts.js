7e21456f0e452530e07d2b655c2dfeba043ed464
AnnaRexha <meldori@outlook.com>
2025-04-25 17:30:18 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-// Example of initializing flip card behavior if you need dynamic actions
-
-// Select all flip cards
-const flipCards = document.querySelectorAll('.flip-card, .about-flip-card');
-
-flipCards.forEach(card => {
-  card.addEventListener('click', () => {
-    const inner = card.querySelector('.flip-inner');
-    inner.classList.toggle('flipped');
-  });
-});
 
 document.querySelector("a[href='#about']").addEventListener("click", function(e) {
   e.preventDefault();
