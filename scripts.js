63c9852a096b048b50c4f9ea10d5740995b6e6a7
AnnaRexha <meldori@outlook.com>
2025-05-01 16:59:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,3 +60,86 @@ document.querySelectorAll(".flip-back").forEach(back => {
     back.style.overflowY = "hidden";  // Hide scrollbar when not hovered
   });
 });
+
+
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"  /* Particle color */
+    },
+    "shape": {
+      "type": "circle",  /* Particle shape */
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    "opacity": {
+      "value": 0.5,  /* Particle transparency */
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.1
+      }
+    },
+    "size": {
+      "value": 5,  /* Size of the particles */
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 4,
+        "size_min": 0.1
+      }
+    },
+    "line_linked": {
+      "enable": true, /* Link particles with lines */
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"  /* Particles will move away when hovered */
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"  /* More particles are added when clicked */
+      }
+    },
+    "modes": {
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      }
+    }
+  },
+  "retina_detect": true
+});
